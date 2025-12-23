9221ae54028d0b56a50dfe59e72c8e4002b6bf71
Vishal Goswami <87699253+MyTricks-code@users.noreply.github.com>
2025-12-23 23:58:09 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,10 +15,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 );
 
 
-// createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
 
